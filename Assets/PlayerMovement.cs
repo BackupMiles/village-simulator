61f3a797211f2599de78f19348535659882731aa
BackupMiles <leanwithmiles@gmail.com>
2024-08-03 22:43:24 +0200

    feat: add sprite renderer

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private Vector2 movement = Vector2.zero;
     [SerializeField] private float moveSpeed;
+
+    private SpriteRenderer sprite;
     private Rigidbody rb;
 
     void OnEnable() {
@@ -21,15 +23,22 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         movement = controls.ReadValue<Vector2>();
+        FlipSprite();
     }
 
     private void FixedUpdate() {
         rb.velocity = new Vector3(movement.x * moveSpeed, 0, movement.y * moveSpeed);
+    }
+
+    private void FlipSprite() {
+        sprite.flipX = movement.x > 0.01 ;
+        
     }
 }
